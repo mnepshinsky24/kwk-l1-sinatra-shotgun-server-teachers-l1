f0c9bf2559a33f996074ad4193357b65e59266a8
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-17 13:25:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,15 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "That's where shotgun comes into play"
-  end 
-  
-  get '/info' do 
-    "Sinatra is cool!"
-  end
-  
-  get '/ava' do
-    "Ava is cool! :)"
-  end
+    
   
 end
